e5c20dadd1181a8362407de229e726ee0acc9340
oleksii.shevchenko1 <oleksii.shevchenko1@nure.ua>
2019-07-24 16:20:51 +0300

    check

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.Pages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SportsStore.Controllers {
+
+    public class HomeController : Controller {
+        [Authorize]
+        public IActionResult IndexUser()
+        {
+            return Content(User.Identity.Name);
+        }
+        private IRepository repository;
+        private ICategoryRepository catRepository;
+
+        public HomeController(IRepository repo, ICategoryRepository catRepo) {
+            repository = repo;
+            catRepository = catRepo;
+        }
+
+        
+
+        public IActionResult Index(QueryOptions options) {
+            return View(repository.GetProducts(options));
+        }
+
+        public IActionResult UpdateProduct(long key) {
+            ViewBag.Categories = catRepository.Categories;
+            return View(key == 0 ? new Product() : repository.GetProduct(key));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product) {
+            if (product.Id == 0) {
+                repository.AddProduct(product);
+            } else {
+                repository.UpdateProduct(product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product) {
+            repository.Delete(product);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
